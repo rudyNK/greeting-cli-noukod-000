2cc84caa68032b0fd52ae57e90a77b0bd3d5c20a
Rudy Ravelin <rudy@noukod.com>
2018-08-30 17:13:59 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,8 @@
-# code the #greeting method here!
-def greeting(name)
-  puts"Hello #{name}!. It's nice to meet you!"
-end
+#!/usr/bin/env ruby
+require_relative "../lib/greeting.rb"
+
+puts "Hi! I'm HAL, what's your name?"
+name = gets.strip
+greeting(name)
+
+# code your CLI here!
